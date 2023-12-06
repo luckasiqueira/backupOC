406eba1eebdbf85fd96b826331f44698bb6a482d
luckasiqueira <lucasguilherme824@gmail.com>
2023-12-06 17:28:49 -0300

    Implemented mysqldump commands

--- controllers/database.go
+++ controllers/database.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 func DbDump(dir string) {
 	// Create new path inside backup directory
@@ -12,5 +16,25 @@ func DbDump(dir string) {
 		panic(err.Error())
 	}
 
+	// Running MySQL dump
+	dump := exec.Command("mysqldump", "-u", "root", "-p"+"", "-h", "", "")
+
+	output, err := dump.Output()
+	if err != nil {
+		panic(err.Error())
+		os.Exit(1)
+	}
+
+	// Set our .sql file as output for mysql dump command
+	dump.Stdout = sqlFile
+
+	// Write data onto .sql file
+	_, err = sqlFile.Write(output)
+	if err != nil {
+		fmt.Printf("Error when writing to .sql file: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Closing .sql file in system
 	defer sqlFile.Close()
 }

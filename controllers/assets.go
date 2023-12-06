0b7c69d38d6575641a64968c0b4724e53499ef1a
luckasiqueira <lucasguilherme824@gmail.com>
2023-12-06 16:35:23 -0300

    Added backup directory creation based on date

---
+++ controllers/assets.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"os"
+	"time"
+)
+
+type Database struct {
+	Name    string
+	Pass    string
+	User    string
+	Host    string
+	Port    string
+	Storage string
+}
+
+func SetFolder() string {
+	// Just gets real time
+	t := time.Now()
+
+	// Set and create our backup directory
+	bkpPath := "backup-" + t.Format("2006.01.02")
+
+	err := os.Mkdir(bkpPath, 755)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return bkpPath
+}
